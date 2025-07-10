edc143709cd1f8f0d614b79816d543a62f0c2436
Chuseok22 <bjh59629@naver.com>
2025-07-10 09:01:40 +0900

    project init

---
+++ src/main/java/com/chuseok22/webdav/WebDavApplication.java
@@ -0,0 +1,13 @@
+package com.chuseok22.webdav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDavApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebDavApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/chuseok22/webdav/WebDavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuseok22.webdav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDavApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
